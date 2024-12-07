c873acb7edc1741cc85b9a0ceea4d324217d85f4
LMU G <2252853883@qq.com>
2024-12-07 21:46:51 +0800

    add new app InputStock and OutputStock,create new data structure

--- Stock/models.py
+++ Stock/models.py
@@ -16,7 +16,7 @@ class Car(models.Model):
 
 class Stock(models.Model):
     Addr = models.CharField(max_length=20)
-    Staff_id = models.IntegerField()
+    Staff_id = models.ForeignKey("Staff",on_delete=models.DO_NOTHING)
     Capacity = models.IntegerField()
     CarStock = models.ManyToManyField(Car,through='Car_Stock')
 
@@ -24,3 +24,13 @@ class Car_Stock(models.Model):
     Car_id = models.ForeignKey("Car",on_delete=models.DO_NOTHING)
     Stock_id = models.ForeignKey("Stock",on_delete=models.DO_NOTHING)
     Num = models.IntegerField()
+
+class Staff(models.Model):
+    Name = models.CharField(max_length=20)
+    Sex = models.CharField(max_length=20)
+    Age = models.IntegerField()
+    Phone = models.CharField(max_length=20)
+    Native_Addr = models.CharField(max_length=20)
+    Salary = models.FloatField()
+    Position = models.CharField(max_length=20)
+    
