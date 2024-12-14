c9fb6617a19cb1e871685a4ca735b2084b84c6dc
LMU G <2252853883@qq.com>
2024-12-14 17:04:46 +0800

    try rest_framework API

--- Basic/views.py
+++ Basic/views.py
@@ -8,8 +8,10 @@ from .models import Car
 from .serializers import car_serializer
 
 # Create your views here.
+
 @api_view()
 def car_list(request):
-    cars = Car.objects.all()
-    serializer = car_serializer(cars,many=True)
-    return Response(serializer.data)
+    return Response("ok")
+    # cars = Car.objects.all()
+    # serializer = car_serializer(cars,many=True)
+    # return Response(serializer.data)
--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+@api_view()
 def say_hello(request):
-    return render(request,'hello.html',{'name':'TOV'})
+    # return render(request,'hello.html',{'name':'TOV'})
+    return Response('Hello World')
     # return HttpResponse('Hello World')
